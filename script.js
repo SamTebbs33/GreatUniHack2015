eea7623303beef26e74ab0f744a12a56aaa61851
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-12-05 17:15:48 +0000

    Attempting to fix api key error

---
+++ script.js
@@ -0,0 +1,9 @@
+var api_key = "AIzaSyB-w-afxayynkmjAAQXUEJfFof29oC9XJQ";
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
